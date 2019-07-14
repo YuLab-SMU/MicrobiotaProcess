e211682ebb81cc2f99b3a7b90bb807b7a2ace744
xiangpin <xshuangbin@163.com>
2019-07-14 20:05:28 +0800

    S4 methods

--- R/getvennlist.R
+++ R/getvennlist.R
@@ -3,25 +3,34 @@
 #' @description
 #' generate a vennlist as the input of \code{[VennDiagram]}.
 #'
-#' @param da dataframe; a dataframe contained one character column and others is numeric 
-#' if sampleinfo is NULL, else a numeirc dataframe.
-#' @param sampleinfo dataframe; a sample information.
+#' @param da dataframe; a dataframe contained one character column and the others are numeric.
+#' all columns should be numeric if sampleinfo isn't NULL.
+#' @param sampleinfo dataframe; a sample information, default is NULL.
+#' @param factorNames character, a column name of sampleinfo,
+#' when sampleinfo isn't NULL, factorNames shouldn't be NULL, default is NULL.
 #' @param ...; Additional arguments passed to \code{\link[MicrobitaProcess]{CountOrRatios}}. 
 #'
 #' @author Shuangbin Xu
 #' @export
 
-getvennlist <- function(da,
+getvennlist.default <- function(da,
 			   sampleinfo=NULL,
+			   factorNames=NULL,
 			   ...
 			   ){
-    	data <- da
-	data <- CountOrRatios(data, 
+	if (!is.null(sampleinfo) && !is.null(factorNames)){
+		sampleinfo <- sampleinfo[,match(factorNames, colnames(sampleinfo)), 
+								 drop=FALSE]
+	}
+	if (!is.null(sampleinfo) && is.null(factorNames)){
+		stop("when sampleinfo isn't NULL, factorNames shouldn't be NULL")
+	}
+	data <- CountOrRatios(da, 
 				featurelist=sampleinfo, 
 				countmode=FALSE,
-				percentmode=FALSE,
 				multiplenum=1,
-   				rownamekeep=FALSE)
+   				rownamekeep=FALSE,
+				...)
 	vennlist <- apply(data, 1, function(data){names(data[data>0])})
 	return(vennlist)
 	
