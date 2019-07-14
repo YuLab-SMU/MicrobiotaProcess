4ed469ce439dbefafc47ab0957aedde1c297ff6f
xiangpin <xshuangbin@163.com>
2019-07-14 20:01:08 +0800

    remove percentmode

--- R/CountOrRatios.R
+++ R/CountOrRatios.R
@@ -3,10 +3,11 @@
 #' @description
 #' Caculate the count or relative abundance of replicate element with a speficify columns
 #' 
-#' @param da dataframe; a dataframe contained one character column and others is numeric 
-#' if featurelist is NULL, else a numeirc dataframe.
+#' @param da dataframe; a dataframe contained one character column and others is numeric,
+#' if featurelist is NULL. Or a numeirc dataframe, if featurelist is non't NULL, all columns 
+#' should be numeric.
 #' @param featurelist dataframe; a dataframe contained one chatacter column, default is NULL.
-#' @param countmode boolean; whether return the count results (FALSE),or relative abundance (TRUE).
+#' @param countmode boolean; whether return the count results (TRUE),or relative abundance (FALSE).
 #' @param percentmode boolean; whether return the percent (FALSE),or between 0 and 1(TRUE).
 #' @param multiplenum numeric; the multiple you want to increase,default is 1.
 #' @param rownamekeep boolean; whether you return a dataframe contained the rownames,default is FALSE.
@@ -15,30 +16,30 @@
 #' @importFrom plyr ddply 
 #' @importFrom plyr numcolwise
 CountOrRatios <- function(da, 
-			     featurelist=NULL, 
-			     countmode=FALSE, 
-			     percentmode=FALSE,
+			     featurelist, 
+			     countmode=TRUE, 
+			     #percentmode=FALSE,
 			     multiplenum=1, 
 			     rownamekeep=FALSE){
 	data <- da
-       if (!is.null(featurelist)){
+    if (!is.null(featurelist)){
 		data <- merge(da, featurelist, by=0)
 		rownames(data) <- data$Row.names
-              data$Row.names <- NULL
+    	data$Row.names <- NULL
 	}
 	nums <- !unlist(lapply(data, is.numeric))
 	group <- names(data[,nums,drop=FALSE])
-       data <- data.frame(plyr::ddply(data, group, plyr::numcolwise(sum)), 
-			     check.names=F, stringsAsFactors=FALSE)   
-       rownames(data) <- as.vector(data[[group]])
-       data[[group]] <- NULL
+    data <- data.frame(plyr::ddply(data, group, plyr::numcolwise(sum)), 
+		     check.names=F, stringsAsFactors=FALSE)   
+    rownames(data) <- as.vector(data[[group]])
+    data[[group]] <- NULL
 	if (!isTRUE(countmode)){
        	data <- data.frame(prop.table(as.matrix(data), 2), check.names=F, stringsAsFactors=FALSE)
 		data <- data*multiplenum
 	}
-      	if (!isTRUE(countmode) && isTRUE(percentmode)){
-       	data <- data*100
-	}
+    #  	if (!isTRUE(countmode) && isTRUE(percentmode)){
+    #   	data <- data*100
+	#}
 	if (isTRUE(rownamekeep)){
 		data <- data.frame(cbind(feature=rownames(data), data), check.names=F, stringsAsFactors=FALSE)
 	}
