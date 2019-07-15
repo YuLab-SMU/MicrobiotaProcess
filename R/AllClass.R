0c38952ccf49553d555f97fb9c24695783865d7d
xiangpin <xshuangbin@163.com>
2019-07-15 10:39:39 +0800

    add pcasample class

--- R/AllClass.R
+++ R/AllClass.R
@@ -1,5 +1,17 @@
 #' @name phyloseq-class
 #' @rdname phyloseq-class
-#' @exportClass phyloseq
-setOldClass("phyloseq")
+#' @importClassesFrom phyloseq phyloseq
+#setOldClass("phyloseq")
+
+#' @importClassesFrom stats prcomp
+setOldClass("prcomp")
+#' @name pcasample-class
+#' @rdname pcasample-class
+#' @exportClass pcasample
+setClass("pcasample",
+		 representation=representation(
+					pca="prcomp",
+					#varcontrib="VarContrib",
+					sampleda="data.frame"),
+		 prototype=prototype(pca=NULL, sampleda=NULL))
 
