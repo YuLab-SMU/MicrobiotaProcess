4b6b59e71d2a9cc5812dd06dcb9145464fa5efa1
xiangpin <xshuangbin@163.com>
2019-08-03 23:02:24 +0800

    rownames to labelnames

--- R/convert.R
+++ R/convert.R
@@ -8,10 +8,11 @@ tidytree::as.treedata
 #' @author Shuangbin Xu
 #' @export
 as.treedata.data.frame <- function(data,...){
+	data <- fillNAtax(data)
 	data <- data.frame(root=rep("r__root", nrow(data)), data)
 	datalist <- list()
-	for (i in 1:(ncol(dat)-1)){
-		tmpdat <- dat[,c(i, i+1)]
+	for (i in 1:(ncol(data)-1)){
+		tmpdat <- data[,c(i, i+1)]
 		colnames(tmpdat) <- c("parent", "child")
 		datalist[[i]] <- tmpdat
 	}
@@ -22,27 +23,27 @@ as.treedata.data.frame <- function(data,...){
 	index[isTip] <- seq(1,sum(isTip))
 	index[!isTip] <- seq(sum(isTip)+2,length(isTip)+1)
 	mapping <- data.frame(node=index, 
-						  row.names=as.vector(datalist$child), 
+						  labelnames=as.vector(datalist$child), 
 						  isTip)
-	mapping$nodeClass <- unlist(lapply(rownames(mapping),
+	mapping$nodeClass <- unlist(lapply(as.vector(mapping$labelnames),
 									   function(x)(unlist(strsplit(x,"__"))[1])))
 	#if (is.null(nodeSize)){
 	mapping$nodeSize <- 1
 	#}
-	parentnode <- mapping[match(as.vector(datalist$parent), rownames(mapping)),]$node 
-	childnode <- mapping[match(as.vector(datalist$child), rownames(mapping)),]$node
+	parentnode <- mapping[match(as.vector(datalist$parent), as.vector(mapping$labelnames)),]$node 
+	childnode <- mapping[match(as.vector(datalist$child), as.vector(mapping$labelnames)),]$node
 	edges <- cbind(parentnode, childnode) 
 	colnames(edges) <- NULL
 	edges[is.na(edges)] <- sum(isTip) + 1 
 	root <- data.frame(node=sum(isTip)+1,
-					   row.names="r__root",
+					   labelnames="r__root",
 					   isTip=FALSE,
 					   nodeClass="r",
 					   nodeSize=1)
 	mapping <- rbind(root, mapping)
 	mapping <- mapping[order(mapping$node),]
-	node.label <- rownames(mapping)[!mapping$isTip]
-	tip.label <- rownames(mapping)[mapping$isTip]
+	node.label <- as.vector(mapping$labelnames)[!mapping$isTip]
+	tip.label <- as.vector(mapping$labelnames)[mapping$isTip]
 	taxphylo <- structure(list(edge=edges,
 							   node.label=node.label,
 							   tip.label=tip.label,
