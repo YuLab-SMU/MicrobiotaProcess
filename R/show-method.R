a4f03a9cc3e884a05e8af12d510c9d14236d7524
xiangpin <xshuangbin@163.com>
2019-08-07 22:22:17 +0800

    diffAnalysisClass show function

---
+++ R/show-method.R
@@ -0,0 +1,46 @@
+#' @title show method
+#' @name show
+#' @docType methods
+#' @rdname show-methods
+#' @param object `diffAnalysisClass` object
+#' @importFrom methods show
+#' @exportMethod show
+#' @author Shuangbin Xu
+#' @return print info
+#' @usage show(object)
+
+setMethod("show", 
+		  signature(object="diffAnalysisClass"),
+		  function(object){
+			originalD <- object@originalD
+		  	cat(paste0("The original data: ", ncol(originalD),
+						 " features and ", nrow(originalD)," samples"),
+				  fill=TRUE)
+			sampleda <- object@sampleda
+			cat(paste0("The sample data: ", ncol(sampleda), " variables and ", nrow(sampleda), " samples"),
+				fill=TRUE)
+			taxda <- object@taxda
+			if(!is.null(taxda)){cat(paste0("The taxda contained ", nrow(taxda), " by ",ncol(taxda), " rank"),
+									fill=TRUE)}
+			else{cat("The taxda is NULL",fill=TRUE)}
+			kwres <- object@kwres
+			numfirstf <- nrow(kwres[kwres$pvalue<=0.05 & !is.na(kwres$pvalue),])
+			cat(paste0("after first test (default is kruskal.test) number of feature (pvalue <=0.05):", 
+					   numfirstf),
+				fill=TRUE)
+			secondvars <- getsecondTRUEvar(object)
+			cat(paste0("after second test (default is wilcox.test) number of significantly discriminative feature:", 
+					   nrow(secondvars)),
+					   fill=TRUE)
+			mlres <- tidydiffAnalysis(object) 
+			uncertain <- length(grep("__un_", mlres$f))
+			cat(paste0("after LDA or rf, Number of discriminative features: ", 
+					   nrow(mlres), " (certain taxonomy classification:", 
+					   nrow(mlres) -uncertain , 
+					   "; uncertain taxonomy classication: ",uncertain,")"), 
+				fill=TRUE)
+		  }
+)
+
+
+
