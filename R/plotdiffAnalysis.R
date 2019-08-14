1ab447b16aa4dbb7704f276961eb0df24c29df79
xiangpin <xshuangbin@163.com>
2019-08-14 17:21:54 +0800

    add examples and reduces lines of function

--- R/plotdiffAnalysis.R
+++ R/plotdiffAnalysis.R
@@ -20,8 +20,28 @@
 #' or set FALSE, then use 'theme' setting.
 #' @param setlegend logical, whether set the legend, default is TRUE,
 #' or set FALSE, then use 'guides' or 'theme' setting.
+#' @param ..., additional parameters.
+#' @return figures of tax clade show the significant different feature.
 #' @author Shuangbin Xu
 #' @export
+#' @examples
+#' data(kostic2012crc)
+#' kostic2012crc
+#' head(phyloseq::sample_data(kostic2012crc),3)
+#' kostic2012crc <- phyloseq::rarefy_even_depth(kostic2012crc,rngseed=1024)
+#' table(sample_data(kostic2012crc)$DIAGNOSIS)
+#' diffres <- diffAnalysis(kostic2012crc, class="DIAGNOSIS",
+#'                         mlfun="lda", filtermod="fdr",
+#'                         firstcomfun = "kruskal.test",
+#'                         firstalpha=0.05, strictmod=TRUE,
+#'                         secondcomfun = "wilcox.test",
+#'                         submin=3, subclwilc=TRUE,
+#'                         secondalpha=0.01, lda=3)
+#' library(ggplot2)
+#' diffcladeplot <- ggdiffclade(diffres,alpha=0.3, size=0.2, 
+#'                         skpointsize=0.4, taxlevel=3,settheme=TRUE,
+#'                         setColors=FALSE) +
+#'                  scale_fill_manual(values=c('#00AED7', '#FD9347', '#C1E168'))
 ggdiffclade <- function(obj,...){
 	UseMethod("ggdiffclade")
 }
@@ -32,66 +52,34 @@ ggdiffclade <- function(obj,...){
 #' @importFrom magrittr %<>%
 #' @importFrom stats as.formula
 #' @export
-ggdiffclade.data.frame <- function(taxda, 
-					 nodedf,
-					 factorName,
-					 layout="circular", 
-					 size=0.6, 
-					 skpointsize=0.8,
-					 alpha=0.4,
-					 taxlevel=6,
-					 cladetext=2,
-					 factorLevels=NULL,
-					 setColors=TRUE,
-					 settheme=TRUE,
-					 setlegend=TRUE,
-					   ...){
+ggdiffclade.data.frame <- function(taxda, nodedf, factorName, layout="circular", size=0.6, 
+					 skpointsize=0.8, alpha=0.4, taxlevel=6, cladetext=2, factorLevels=NULL, setColors=TRUE,
+					 settheme=TRUE, setlegend=TRUE, ...){
 	treedata <- convert_to_treedata(taxda)
 	layout %<>% match.arg(c("rectangular", "circular"))
-	if (!is.null(factorLevels)){
-		nodedf <- setfactorlevels(nodedf, factorLevels)
-	}
+	if (!is.null(factorLevels)){nodedf <- setfactorlevels(nodedf, factorLevels)}
 	nodedf <- getnode(treedata, nodedf)
-	p <- treeskeleton(treedata,
-				layout=layout,
-				size=size,
-				pointsize=skpointsize)
+	p <- treeskeleton(treedata,	layout=layout,size=size,pointsize=skpointsize)
 	cladecoord <- getcladedf(p, nodedf$node)
 	cladecoord <- merge(cladecoord, nodedf, by.x="node", by.y="node")
 	labelannotcoord <- getlabeldf(p, nodedf$node)
 	if (layout=="rectangular"){taxlevel=7}
-	labelannotcoord <- getannotlabel(labelannotcoord, 
-									 classlevel=taxlevel)
+	labelannotcoord <- getannotlabel(labelannotcoord, classlevel=taxlevel)
 	labelcoord <- labelannotcoord$labeldf
 	annotcoord <- labelannotcoord$annotdf
 	if ("pvalue" %in% colnames(nodedf)){
 		nodedf[["-log10(pvalue)"]] <- -log10(nodedf$pvalue)
-		pointmapping <- aes_(x=~x,y=~y,
-							 fill=as.formula(paste("~",factorName)), 
-							 size=~-log10(pvalue))
+		pointmapping <- aes_(x=~x,y=~y,fill=as.formula(paste("~",factorName)), size=~-log10(pvalue))
 	}else{
-		pointmapping <- aes_(x=~x,y=~y,
-							 fill=as.formula(paste("~",factorName)))
+		pointmapping <- aes_(x=~x,y=~y, fill=as.formula(paste("~",factorName)))
 	}
-	p <- p + geom_rect(data=cladecoord,
-					   aes_(fill=as.formula(paste("~",factorName)),
-						   xmin=~xmin,
-						   ymin=~ymin,
-						   xmax=~xmax,
-						   ymax=~ymax),
+	p <- p + geom_rect(data=cladecoord,aes_(fill=as.formula(paste("~",factorName)), 
+											xmin=~xmin, ymin=~ymin, xmax=~xmax, ymax=~ymax),
 					   alpha=alpha,
 					   show.legend=FALSE) +
-		geom_text(data=labelcoord,
-				  aes(x=x,y=y,
-					  label=label,
-					  angle=angle),
-				  size=2) +
-		geom_point(data=cladecoord,
-				   pointmapping,
-				   shape=21)+
-		geom_point(data=annotcoord, 
-				   aes(x=0,y=0, color=label),
-				   size=0, stroke=0) +
+		geom_text(data=labelcoord, aes_(x=~x,y=~y, label=~label, angle=~angle), size=2) +
+		geom_point(data=cladecoord, pointmapping, shape=21)+
+		geom_point(data=annotcoord, aes_(x=0,y=0, color=~label), size=0, stroke=0) +
 		scale_size_continuous(range = c(1, 3))
 	if (setColors){
 		tmpn <- length(unique(as.vector(nodedf[[match(factorName,colnames(nodedf))]])))
@@ -136,7 +124,7 @@ ggdiffclade.diffAnalysisClass <- function(obj, removeUnkown=TRUE, ...){
 
 #' @title significantly discriminative feature barplot
 #' @param obj object, diffAnalysisClass see also \code{\link[MicrobiotaProcess]{diffAnalysis}} 
-#' or featureMeanMedian see also \code{\link[MicrobiotaProcess]{getMeanMedian}}.
+#' @param feMeanMedian object, featureMeanMedian see also \code{\link[MicrobiotaProcess]{getMeanMedian}}.
 #' @param filepath character, default is NULL, meaning current path. 
 #' @param output character, the output dir name, default is "biomarker_barplot".
 #' @param xtextsize numeric, the size of axis x label, default is 3.
@@ -146,12 +134,32 @@ ggdiffclade.diffAnalysisClass <- function(obj, removeUnkown=TRUE, ...){
 #' @param featurename character, the feature name, contained at the objet.
 #' @param class character, factor name.
 #' @param subclass character, factor name. 
+#' @param factorLevels list,  the levels of the factors, default is NULL,
+#' if you want to order the levels of factor, you can set this. 
 #' @param setColors logical, whether set the colors, default is TRUE, or FALSE,then
 #' use scale_color_manual setting.
+#' @param coloslist vector, color vector, if the input is phyloseq, you should use this to adjust the color, 
+#' not scale_color_manual.
 #' @param settheme logical, whether set the theme, default is TRUE, or FALSE, then
 #' use theme and guides setting.
+#' @return the figures of features show the distributions in samples.
 #' @author Shuangbin Xu
 #' @export
+#' @examples
+#' data(kostic2012crc)
+#' kostic2012crc
+#' head(phyloseq::sample_data(kostic2012crc),3)
+#' kostic2012crc <- phyloseq::rarefy_even_depth(kostic2012crc,rngseed=1024)
+#' table(sample_data(kostic2012crc)$DIAGNOSIS)
+#' diffres <- diffAnalysis(kostic2012crc, class="DIAGNOSIS",
+#'                         mlfun="lda", filtermod="fdr",
+#'                         firstcomfun = "kruskal.test",
+#'                         firstalpha=0.05, strictmod=TRUE,
+#'                         secondcomfun = "wilcox.test",
+#'                         submin=3, subclwilc=TRUE,
+#'                         secondalpha=0.01, lda=3)
+#' # not run in example
+#' #ggdifftaxbar(diffres, output="./biomarker_barplot")
 ggdifftaxbar <- function(obj,...){
 	UseMethod("ggdifftaxbar")
 }
@@ -161,6 +169,7 @@ setGeneric("ggdifftaxbar")
 
 #' @aliases ggdifftaxbar,diffAnalysisClass
 #' @rdname ggdifftaxbar
+#' @importFrom ggplot2 ggsave
 #' @export
 setMethod("ggdifftaxbar","diffAnalysisClass",function(obj,
 										   filepath=NULL,
@@ -207,16 +216,8 @@ setMethod("ggdifftaxbar","diffAnalysisClass",function(obj,
 #' @importFrom ggplot2 aes geom_errorbar scale_linetype_manual unit theme_bw scale_y_continuous labs xlab facet_grid
 #' @importFrom ggplot2 guide_legend element_text element_rect element_blank scale_fill_manual
 #' @export
-ggdifftaxbar.featureMeanMedian <- function(feMeanMedian, 
-									featurename, 
-									class, 
-									subclass,
-									xtextsize=3,
-									factorLevels=NULL,
-									setColors=TRUE, 
-									coloslist=NULL,
-									settheme=TRUE,
-									...){
+ggdifftaxbar.featureMeanMedian <- function(feMeanMedian, featurename, class, subclass, xtextsize=3,
+									factorLevels=NULL, setColors=TRUE, coloslist=NULL, settheme=TRUE, ...){
 	data <- feMeanMedian$singlefedf
 	dastatistic <- feMeanMedian$singlefestat
 	if (!is.null(factorLevels)){
@@ -224,36 +225,20 @@ ggdifftaxbar.featureMeanMedian <- function(feMeanMedian,
 		dastatistic <- setfactorlevels(dastatistic, factorLevels)
 	}
 	if (missing(subclass)){subclass <- class}
-	p <- ggplot(data, aes_(x=~sample, 
-						   y=~RelativeAbundance,
-						   fill=as.formula(paste0("~",subclass))))+
+	p <- ggplot(data, aes_(x=~sample, y=~RelativeAbundance, fill=as.formula(paste0("~",subclass))))+
 		geom_bar(stat="identity") +
-		geom_errorbar(data=dastatistic,
-					  aes_(x=~sample,
-						  ymax=~value,
-						  ymin=~value,
-						  linetype=~statistic),
-					  size=0.5, width=1, 
-					  inherit.aes=FALSE)+
+		geom_errorbar(data=dastatistic, aes_(x=~sample, ymax=~value, ymin=~value, linetype=~statistic), size=0.5, width=1, inherit.aes=FALSE)+
 		scale_linetype_manual(values=c("solid", "dotted"))+
-		facet_grid(as.formula(paste0("~",class)),
-				   space="free_x", scales="free_x") + 
+		facet_grid(as.formula(paste0("~",class)), space="free_x", scales="free_x") + 
 		labs(title=featurename) + xlab(NULL)+
-		scale_y_continuous(expand=c(0,0),
-						   limits=c(0,max(data$RelativeAbundance)*1.05))
+		scale_y_continuous(expand=c(0,0), limits=c(0,max(data$RelativeAbundance)*1.05))
 	if (settheme){
-		p <- p + 
-				theme_bw() + 
-				guides(fill= guide_legend(keywidth = 0.5, keyheight = 0.5, order=1),
+		p <- p + theme_bw() + guides(fill= guide_legend(keywidth = 0.5, keyheight = 0.5, order=1),
 					   linetype=guide_legend(keywidth = 0.7, keyheight = 0.5, order=2))+
 				theme(plot.title = element_text(face="bold",lineheight=25,hjust=0.5),
-					  panel.grid=element_blank(),
-					  legend.text = element_text(size=6.5),
-					  legend.title=element_text(size=7),
-					  legend.background=element_rect(fill=NA),
-					  axis.text.x=element_text(angle=-45, hjust = 0, size=xtextsize),
-					  panel.spacing = unit(0.2, "mm"), 
-					  strip.background = element_rect(colour=NA,fill="grey"))
+					  panel.grid=element_blank(), legend.text = element_text(size=6.5),  legend.title=element_text(size=7),
+					  legend.background=element_rect(fill=NA), axis.text.x=element_text(angle=-45, hjust = 0, size=xtextsize),
+					  panel.spacing = unit(0.2, "mm"), strip.background = element_rect(colour=NA,fill="grey"))
 	}
 	if (setColors){
 		if (is.null(coloslist)){
@@ -274,11 +259,26 @@ ggdifftaxbar.featureMeanMedian <- function(feMeanMedian,
 #' @importFrom dplyr rename group_by_ mutate
 #' @importFrom magrittr %>%
 #' @importFrom stats median
+#' @return featureMeanMedian object, contained the abundance of feature, and the 
+#' mean and median of feature by subclass.
 #' @author Shuangbin Xu
 #' @export
+#' @examples
+#' data(hmp_aerobiosis_small)
+#' head(sampleda)
+#' featureda <- merge(featureda, sampleda, by=0)
+#' rownames(featureda) <- as.vector(featureda$Row.names)
+#' featureda$Row.names <- NULL
+#' feameamed <- getMeanMedian(datameta=featureda, 
+#'                     feature="p__Actinobacteria", 
+#'                     subclass="body_site")
+#' #not run in example
+#' #fplot <- ggdifftaxbar(feameamed, featurename="p__Actinobacteria", 
+#' #                     class="oxygen_availability", subclass="body_site")
 getMeanMedian <- function(datameta, feature, subclass){
 	#featureda <- merge(data,sampleda,by=0) %>% rename(sample=Row.names)
-	factornames <- colnames(datameta)[!unlist(sapply(datameta,is.numeric))]
+	RelativeAbundance <- NULL
+	factornames <- colnames(datameta)[!unlist(vapply(datameta,is.numeric,logical(1)))]
 	featuredatmp <- datameta %>% rownames_to_column(var="sample") %>%
 			select(c("sample", feature, factornames)) %>%
 			rename(RelativeAbundance=feature) %>% 
@@ -300,12 +300,39 @@ getMeanMedian <- function(datameta, feature, subclass){
 #' @param data data.frame, data.frame contained effect size and the group information. 
 #' @param factorName character, the column name contained group information in data.frame.
 #' @param effectsizename character, the column name contained effect size information.
+#' @param factorLevels list, the levels of the factors, default is NULL,
+#' if you want to order the levels of factor, you can set this.
 #' @param setColors logical, whether set the colors, default is TRUE, or FALSE,then 
 #' use scale_color_manual setting.
 #' @param settheme logical, whether set the theme, default is TRUE, or FALSE, then
 #' use theme and guides setting.
+#' @return the figures of effect size show the LDA or MeanDecreaseAccuracy.
 #' @author Shuangbin Xu
 #' @export
+#' @examples
+#' data(kostic2012crc)
+#' kostic2012crc
+#' head(phyloseq::sample_data(kostic2012crc),3)
+#' kostic2012crc <- phyloseq::rarefy_even_depth(kostic2012crc,rngseed=1024)
+#' table(sample_data(kostic2012crc)$DIAGNOSIS)
+#' diffres <- diffAnalysis(kostic2012crc, class="DIAGNOSIS",
+#'                         mlfun="lda", filtermod="fdr",
+#'                         firstcomfun = "kruskal.test",
+#'                         firstalpha=0.05, strictmod=TRUE,
+#'                         secondcomfun = "wilcox.test", 
+#'                         submin=3, subclwilc=TRUE,
+#'                         secondalpha=0.01, lda=3)  
+#' effectplot <- ggeffectsize(diffres,
+#'                         setColors=FALSE,
+#'                         settheme=FALSE) +
+#'               scale_color_manual(values=c('#00AED7', 
+#'                                           '#FD9347', 
+#'                                           '#C1E168'))+
+#'               theme_bw()+
+#'               theme(strip.background=element_rect(fill=NA),
+#'                     panel.spacing = unit(0.2, "mm"),
+#'                     panel.grid=element_blank(),
+#'                     strip.text.y=element_blank())
 ggeffectsize <- function(obj,...){
 	UseMethod("ggeffectsize")
 }
@@ -333,7 +360,7 @@ ggeffectsize.data.frame <- function(data,
 	p <- ggplot(data=data, 
 				aes_(x=as.formula(paste0("~",effectsizename)),
 					 y=~f)) + 
-		geom_segment(aes(xend=0, yend=f), 
+		geom_segment(aes_(xend=0, yend=~f), 
 					 color="grey") + 
 		geom_point(aes_(color=as.formula(paste0("~",factorName)))) +
 		facet_grid(as.formula(paste0(factorName," ~.")),
