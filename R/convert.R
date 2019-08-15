7b3d0972f15c8342c36312ad13c40c8995729f76
xiangpin <xshuangbin@163.com>
2019-08-15 21:47:50 +0800

    change tab to 4 spaces

--- R/convert.R
+++ R/convert.R
@@ -10,37 +10,37 @@
 #' head(taxda)
 #' treedat <- convert_to_treedata(taxda)
 convert_to_treedata <- function(data,...){
-	data <- fillNAtax(data)
-	data <- data.frame(root=rep("r__root", nrow(data)), data)
-	datalist <- list()
-	for (i in seq_len(ncol(data)-1)){
-		tmpdat <- data[,c(i, i+1)]
-		colnames(tmpdat) <- c("parent", "child")
-		datalist[[i]] <- tmpdat
-	}
-	datalist <- do.call("rbind", datalist)
-	datalist <- datalist[!duplicated(datalist),]
-	isTip <- !as.vector(datalist$child) %in% as.vector(datalist$parent)
-	index <- c()
-	index[isTip] <- seq(1,sum(isTip))
-	index[!isTip] <- seq(sum(isTip)+2,length(isTip)+1)
-	mapping <- data.frame(node=index, labelnames=as.vector(datalist$child), isTip)
-	mapping$nodeClass <- unlist(lapply(as.vector(mapping$labelnames),
-									   function(x)(unlist(strsplit(x,"__"))[1])))
-	mapping$nodeSize <- 1
-	parentnode <- mapping[match(as.vector(datalist$parent), as.vector(mapping$labelnames)),]$node 
-	childnode <- mapping[match(as.vector(datalist$child), as.vector(mapping$labelnames)),]$node
-	edges <- cbind(parentnode, childnode) 
-	colnames(edges) <- NULL
-	edges[is.na(edges)] <- sum(isTip) + 1
-	root <- data.frame(node=sum(isTip)+1,labelnames="r__root",
-					   isTip=FALSE, nodeClass="r", nodeSize=1)
-	mapping <- rbind(root, mapping)
-	mapping <- mapping[order(mapping$node),]
-	node.label <- as.vector(mapping$labelnames)[!mapping$isTip]
-	tip.label <- as.vector(mapping$labelnames)[mapping$isTip]
-	taxphylo <- structure(list(edge=edges, node.label=node.label,
-							   tip.label=tip.label, edge.length=rep(0.5, nrow(edges)),
-							   Nnode = length(node.label)), class="phylo") 
-	res <- new("treedata", phylo=taxphylo, data=as_tibble(mapping))
+    data <- fillNAtax(data)
+    data <- data.frame(root=rep("r__root", nrow(data)), data)
+    datalist <- list()
+    for (i in seq_len(ncol(data)-1)){
+    	tmpdat <- data[,c(i, i+1)]
+    	colnames(tmpdat) <- c("parent", "child")
+    	datalist[[i]] <- tmpdat
+    }
+    datalist <- do.call("rbind", datalist)
+    datalist <- datalist[!duplicated(datalist),]
+    isTip <- !as.vector(datalist$child) %in% as.vector(datalist$parent)
+    index <- c()
+    index[isTip] <- seq(1,sum(isTip))
+    index[!isTip] <- seq(sum(isTip)+2,length(isTip)+1)
+    mapping <- data.frame(node=index, labelnames=as.vector(datalist$child), isTip)
+    mapping$nodeClass <- unlist(lapply(as.vector(mapping$labelnames),
+    								   function(x)(unlist(strsplit(x,"__"))[1])))
+    mapping$nodeSize <- 1
+    parentnode <- mapping[match(as.vector(datalist$parent), as.vector(mapping$labelnames)),]$node 
+    childnode <- mapping[match(as.vector(datalist$child), as.vector(mapping$labelnames)),]$node
+    edges <- cbind(parentnode, childnode) 
+    colnames(edges) <- NULL
+    edges[is.na(edges)] <- sum(isTip) + 1
+    root <- data.frame(node=sum(isTip)+1,labelnames="r__root",
+    				   isTip=FALSE, nodeClass="r", nodeSize=1)
+    mapping <- rbind(root, mapping)
+    mapping <- mapping[order(mapping$node),]
+    node.label <- as.vector(mapping$labelnames)[!mapping$isTip]
+    tip.label <- as.vector(mapping$labelnames)[mapping$isTip]
+    taxphylo <- structure(list(edge=edges, node.label=node.label,
+    						   tip.label=tip.label, edge.length=rep(0.5, nrow(edges)),
+    						   Nnode = length(node.label)), class="phylo") 
+    res <- new("treedata", phylo=taxphylo, data=as_tibble(mapping))
 }
