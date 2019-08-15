b6444c982bc00ee1b98317289ea31284ab20128c
xiangpin <xshuangbin@163.com>
2019-08-15 22:14:16 +0800

    rm test-conversion.R

--- tests/testthat/test-conversion.R
+++
@@ -1,6 +0,0 @@
-context("convert_to_treedata")
-test_that("data to treedata",{
-    data(hmp_aerobiosis_small)
-    treeda <- convert_to_treedata(taxda)
-    expect_that(treeda, is_a("treedata"))
-})
