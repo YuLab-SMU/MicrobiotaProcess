172ab134d5d66c2d7dbcb48941ab59ef2dddf493
xiangpin <xshuangbin@163.com>
2019-08-15 21:53:06 +0800

    tab to 4 spaces

--- R/getvennlist.R
+++ R/getvennlist.R
@@ -1,25 +1,23 @@
 #' @method getvennlist default
 #' @rdname getvennlist
 #' @export
-getvennlist.default <- function(data,
-			   sampleinfo=NULL,
-			   factorNames=NULL,
-			   ...
-			   ){
-	if (!is.null(sampleinfo) && !is.null(factorNames)){
-		sampleinfo <- sampleinfo[,match(factorNames, colnames(sampleinfo)), 
-								 drop=FALSE]
-	}
-	if (!is.null(sampleinfo) && is.null(factorNames)){
-		stop("when sampleinfo isn't NULL, factorNames shouldn't be NULL")
-	}
-	data <- CountOrRatios(data, 
-				featurelist=sampleinfo, 
-				countmode=FALSE,
-				multiplenum=1,
-   				rownamekeep=FALSE,
-				...)
-	vennlist <- apply(data, 1, function(x){names(x[x>0])})
-	return(vennlist)
-	
+getvennlist.default <- function(obj,
+    sampleinfo=NULL,
+    factorNames=NULL,
+    ...){
+    if (!is.null(sampleinfo) && !is.null(factorNames)){
+    	sampleinfo <- sampleinfo[,match(factorNames, colnames(sampleinfo)), 
+    							 drop=FALSE]
+    }
+    if (!is.null(sampleinfo) && is.null(factorNames)){
+    	stop("when sampleinfo isn't NULL, factorNames shouldn't be NULL")
+    }
+    obj <- CountOrRatios(obj, 
+    			featurelist=sampleinfo, 
+    			countmode=FALSE,
+    			multiplenum=1,
+    			rownamekeep=FALSE,
+    			...)
+    vennlist <- apply(obj, 1, function(x){names(x[x>0])})
+    return(vennlist)
 }
