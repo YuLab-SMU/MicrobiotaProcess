69941ff7bf246c4c74a84693bdc9a15f4c092e7c
xiangpin <xshuangbin@163.com>
2019-08-15 22:22:22 +0800

    modified the examples

--- R/droptax.R
+++ R/droptax.R
@@ -1,15 +1,19 @@
 #' @title Dropping Species with Few abundance and Few Occurrences
 #' 
 #' @description
-#' Drop species or features from the feature data frame that occur fewer than or equal to a threshold number of 
-#' occurrences and fewer abundance than to a threshold abundance.
+#' Drop species or features from the feature data frame that occur 
+#' fewer than or equal to a threshold number of occurrences and fewer 
+#' abundance than to a threshold abundance.
 #'
-#' @param taxtab dataframe; a dataframe of species (or features), default is (n_sample, n_feature).
+#' @param taxtab dataframe; a dataframe of species (or features), 
+#' default is (n_sample, n_feature).
 #' @param rmode boolean; whether transpose the taxtab, default is False.
-#' @param minocc numeric; the threshold number of occurrences to be dropped, if < 1.0,
-#' it will be the threshold ratios of occurrences, default is 0.
-#' @param minabu numeric: the threshold abundance, if fewer than the threshold will be dropped, default is 0.
-#' @return a list contained feature dataframe dropped, and the call, arguments.
+#' @param minocc numeric; the threshold number of occurrences to be 
+#' dropped, if < 1.0,it will be the threshold ratios of occurrences, 
+#' default is 0.
+#' @param minabu numeric: the threshold abundance, if fewer than the 
+#' threshold will be dropped, default is 0.
+#' @return dataframe of new features.
 #' @export
 #' @author Shuangbin Xu
 #' @examples
@@ -21,16 +25,16 @@
 #'                     comment.char="")
 #' otuda <- otuda[sapply(otuda, is.numeric)]
 #' dim(otuda)
-#' otudat <- droptax(otuda, minocc=0.2, minabu=10)
+#' otudat <- droptax(otuda, rmode=TRUE, minocc=0.1, minabu=1)
 #' dim(otudat)
 droptax <- function(taxtab, rmode=FALSE, minocc=0, minabu=0){
-	if (isTRUE(rmode)){
-		taxtab <- data.frame(t(taxtab), check.names=FALSE)
-	}
-	if (minocc < 1.0){
-		minocc <- round(dim(taxtab)[1]*minocc, 0)
-	}
-	taxtab <- taxtab[,apply(taxtab>minabu,2,sum)>=minocc]
-	return (taxtab)
+    if (isTRUE(rmode)){
+    	taxtab <- data.frame(t(taxtab), check.names=FALSE)
+    }
+    if (minocc < 1.0){
+    	minocc <- round(dim(taxtab)[1]*minocc, 0)
+    }
+    taxtab <- taxtab[,apply(taxtab>minabu,2,sum)>=minocc]
+    return (taxtab)
 }
 
