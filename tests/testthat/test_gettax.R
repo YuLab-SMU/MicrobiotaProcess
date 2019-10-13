3ca1ff0b7ced1f628a798d59042315171a4bb3c4
xiangpin <xshuangbin@163.com>
2019-10-13 17:41:53 +0800

    changed get_taxdf to get_taxadf

--- tests/testthat/test_gettax.R
+++ tests/testthat/test_gettax.R
@@ -1,8 +1,8 @@
-context("get_taxdf work")
-test_that("get_taxdf work",{
+context("get_taxadf work")
+test_that("get_taxadf work",{
     library(phyloseq)
     dir <- system.file("extdata", package="phyloseq")
     richph <- import_biom(file.path(dir,"rich_dense_otu_table.biom"))
-    phyda <- get_taxdf(richph, taxlevel=2)
+    phyda <- get_taxadf(richph, taxlevel=2)
     expect_true(is(phyda, "phyloseq"))
 })  
