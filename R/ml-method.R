dbb3a91380e7765bbdc109ebfe3468e4e9bdb780
xiangpin <xshuangbin@163.com>
2019-11-04 09:42:45 +0800

    default replace formula in randomForest

--- R/ml-method.R
+++ R/ml-method.R
@@ -5,25 +5,25 @@ LDAeffectsize <- function(datalist, compareclass, class, bootnums=30, LDA=2){
     res <- list()
     tmpformula <- as.formula(paste0(class, " ~. "))
     for (i in seq_len(bootnums)){
-    	compareres <- list()
-    	df <- datalist[[i]]
-    	for (p in seq_len(nrow(compareclass))){
-    		tmppairs <- compareclass[p,]
-    		z <- suppressWarnings(lda(tmpformula,data=df, tol=1e-6))
-    		w <- z$scaling[,1]
-    		w.unit <- w/sqrt(sum(w^2))
-    		ss <- df %>% select(-c(class)) %>% as.matrix()
-    		LD <- ss %*% w.unit
-    		tmpp1 <- df[[match(class, colnames(df))]]==tmppairs[1]
-    		tmpp2 <- df[[match(class, colnames(df))]]==tmppairs[2]
-    		effect.size <- abs(mean(LD[tmpp1,])-mean(LD[tmpp2,]))
-    		wfinal <- w.unit * effect.size
-    		coeff <- abs(wfinal)
-    		mm <- z$means
-    		gm <- abs(mm[match(tmppairs[1], rownames(mm)),,drop=FALSE]- mm[match(tmppairs[2], rownames(mm)),,drop=FALSE])
-    		tmpres <- (gm+coeff) *0.5
-    		compareres[[p]] <- tmpres
-    	}
+    compareres <- list()
+    df <- datalist[[i]]
+    for (p in seq_len(nrow(compareclass))){
+        tmppairs <- compareclass[p,]
+        z <- suppressWarnings(lda(tmpformula,data=df, tol=1e-6))
+        w <- z$scaling[,1]
+        w.unit <- w/sqrt(sum(w^2))
+        ss <- df %>% select(-c(class)) %>% as.matrix()
+        LD <- ss %*% w.unit
+        tmpp1 <- df[[match(class, colnames(df))]]==tmppairs[1]
+        tmpp2 <- df[[match(class, colnames(df))]]==tmppairs[2]
+        effect.size <- abs(mean(LD[tmpp1,])-mean(LD[tmpp2,]))
+        wfinal <- w.unit * effect.size
+        coeff <- abs(wfinal)
+        mm <- z$means
+        gm <- abs(mm[match(tmppairs[1], rownames(mm)),,drop=FALSE]- mm[match(tmppairs[2], rownames(mm)),,drop=FALSE])
+        tmpres <- (gm+coeff) *0.5
+        compareres[[p]] <- tmpres
+    }
     compareres <- do.call("rbind", compareres)
     res[[i]] <- compareres
     }
@@ -38,20 +38,25 @@ LDAeffectsize <- function(datalist, compareclass, class, bootnums=30, LDA=2){
     return(res)
 }
 
-#' @importFrom randomForest randomForest
+#' @importFrom randomForest randomForest importance
 #' @keywords internal
-rfimportance <- function(datalist, class, bootnums){
+rfimportance <- function(datalist, class, bootnums, effsize=2){
     rfres <- list()
-    tmpformula <- as.formula(paste0(class, " ~. "))
+    #tmpformula <- as.formula(paste0(class, " ~. "))
     for (i in seq_len(bootnums)){
-    	df <- datalist[[i]]
-    	dfres <- randomForest(tmpformula, data=df, importance=TRUE, proximity=TRUE)
-    	imres <- dfres$importance	
-    	rfres[[i]] <- imres
+        df <- datalist[[i]]
+        classindex <- match(class, colnames(df))
+        X <- df[,-classindex]
+        X <- apply(X, 2, function(x)(x-min(x))/(max(x)-min(x)))
+        Y <- df[, classindex]
+        dfres <- randomForest(x=X, y=Y, importance=TRUE, proximity=TRUE)
+        imres <- importance(dfres, type=1)
+        rfres[[i]] <- imres
     }
     rfres <- Reduce("+", rfres)
     rfres <- data.frame(rfres/bootnums)
     rfres <- data.frame(f=rownames(rfres), MeanDecreaseAccuracy=rfres$MeanDecreaseAccuracy)
+    rfres <- rfres[rfres$MeanDecreaseAccuracy>=effsize,]
     return(rfres)
 }
 
@@ -75,9 +80,9 @@ sampledflist <- function(dalist,
     makerownames=FALSE){
     datalist <- list()
     for (i in seq_len(bootnums)){
-    	res <- lapply(dalist,function(x){x[sample(nrow(x), trunc(nrow(x)*ratio)),,drop=FALSE]})
-    	res <- do.call("rbind", c(res, make.row.names=makerownames))
-    	datalist[[i]] <- res
+        res <- lapply(dalist,function(x){x[sample(nrow(x), trunc(nrow(x)*ratio)),,drop=FALSE]})
+        res <- do.call("rbind", c(res, make.row.names=makerownames))
+        datalist[[i]] <- res
     }
     return(datalist)
 }
