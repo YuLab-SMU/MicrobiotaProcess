c30ceacf66ec1fc477286b6b70609642aa2b5b76
xiangpin <xshuangbin@163.com>
2021-08-13 21:55:21 +0800

    add options to change radial.line.type to curved

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onAttach <- function(libname, pkgname) {
+    #pkgVersion <- packageDescription(pkgname, fields="Version")
+    #
+    #citation <- paste0("If you use ", pkgname,
+    #                   " in published research, please cite the paper:\n\n",
+    #                   MP_citations(), "\n")
+
+    #packageStartupMessage(paste0(msg, citation))
+    options(radial.line.type="curved")
+}
