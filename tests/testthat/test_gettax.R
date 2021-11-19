d21a0b35aef019eee6d2491370d54bcecd148dc8
xiangpin <xshuangbin@163.com>
2021-11-19 20:46:18 +0800

    remove phyloseq class

--- tests/testthat/test_gettax.R
+++ tests/testthat/test_gettax.R
@@ -1,8 +1,18 @@
-context("get_taxadf work")
-test_that("get_taxadf work",{
-    library(phyloseq)
-    dir <- system.file("extdata", package="phyloseq")
-    richph <- import_biom(file.path(dir,"rich_dense_otu_table.biom"))
-    phyda <- get_taxadf(richph, taxlevel=2)
-    expect_true(is(phyda, "phyloseq"))
+context("mp_cal_abundance and mp_extract_abundance work")
+test_that("mp_cal_abundance and mp_extract_abundance work",{
+    data(test_otu_data)
+    otuda <- test_otu_data %>% 
+             mp_extract_assays(.abundance=Abundance)
+    taxada <- test_otu_data %>%
+              mp_extract_taxonomy() %>%
+              tibble::column_to_rownames(var="OTU")
+    phyla1 <- test_otu_data %>% 
+        mp_cal_abundance(.abundance=Abundance, force=TRUE) %>% 
+        mp_extract_abundance(taxa.class=Phylum) %>% 
+        tidyr::unnest(AbundanceBySample) %>% 
+        select(label, Sample, RelAbundanceBySample) %>% 
+        tidyr::pivot_wider(id_cols="label", names_from=Sample, values_from=RelAbundanceBySample) %>% 
+        tibble::column_to_rownames(var="label")
+    phyla2 <- get_ratio(otuda, taxada[, 2, drop=FALSE])
+    testthat::expect_equal(phyla1, phyla2 * 100)
 })  
--- tests/testthat/test_getvennlist.R
+++ tests/testthat/test_getvennlist.R
@@ -1,6 +1,5 @@
-context("getvennlist work")
+context("mp_cal_venn and getvennlist work")
 test_that("getvennlist work",{
-	library(phyloseq)
 	dir <- system.file("extdata", package="MicrobiotaProcess")
 	otuda <- read.table(file.path(dir,"otu_tax_table.txt"),
 						row.names=1, check.names=FALSE, skip=1,
@@ -10,8 +9,10 @@ test_that("getvennlist work",{
 	sampleda <- read.table(file.path(dir, "sample_info.txt"), 
 						   header=TRUE, row.names=1)
 	vennlist1 <- get_vennlist(obj=otuda, sampleinfo=sampleda, factorNames="group")
-	testph <- import_qiime(file.path(dir,"otu_tax_table.txt"),
-						   file.path(dir, "sample_info.txt")) 
-	vennlist2 <- get_vennlist(testph, factorNames="group")
+	testph <- mp_import_qiime(file.path(dir,"otu_tax_table.txt"),
+						   file.path(dir, "sample_info.txt"))
+    vennlist2 <- testph %>% 
+                 mp_cal_venn(.abundance=Abundance, .group=group, action="get", force=TRUE) %>% 
+                 dplyr::pull(vennOfgroup, name=group)
 	expect_equal(vennlist1, vennlist2)
 })
